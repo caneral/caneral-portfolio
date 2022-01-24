308aaea612ff426db841bab1748693c99e6c57d6
Caner Al <caneral5858@gmail.com>
2022-01-24 11:48:45 +0300

    change

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,7 +4,13 @@ const Homepage = () => {
   return (
     <div className="flex min-h-screen p-14">
       <div className="bg-gray-600 w-full">
-          Caner Al
+          <div className="grid grid-rows-4 sm:grid-rows-4 md:grid-rows-3 lg:grid-rows-1 xl:grid-rows-1 lg:grid-cols-2  h-full">
+            <div className="bg-portfolio flex  justify-center">
+            <div className="my-4 md:mt-16 sm:mt-12 lg:my-72 w-3/4 ">
+                Caner
+                </div>
+            </div>
+          </div>
       </div>
     </div>
   );
