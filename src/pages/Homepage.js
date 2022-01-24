537916e973f3cff347ffcc449e5123520633b09a
Caner Al <caneral5858@gmail.com>
2022-01-24 13:36:25 +0300

    Page edited

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,23 @@
 import React from "react";
+import Typical from "react-typical";
+import {
+    AwesomeButtonProgress,
+} from 'react-awesome-button';
+import "react-awesome-button/dist/styles.css";
+import cv from '../assets/cv/caneral_cv.pdf';
+import caneral from '../assets/IMG_7464.jpeg';
 
 const Homepage = () => {
+
+    const doSomethingThenCall = () => {
+        const link = document.createElement('a');
+        link.href = cv;
+        link.download = "Caner_Al_CV";
+        document.body.appendChild(link);
+        link.click();
+    }
+
+
   return (
     <div className="flex min-h-screen p-14">
       <div className="bg-gray-600 w-full">
@@ -16,8 +33,39 @@ const Homepage = () => {
               <p className="text-white md:text-5xl sm:text-3xl lg:text-7xl font-bold text-3xl">
                 CANER AL
               </p>
+              <p className="text-white md:text-3xl sm:text-xl lg:text-5xl font-bold">
+                <Typical
+                  loop={Infinity}
+                  wrapper="b"
+                  steps={[
+                    "Information Systems Engineer",
+                    2000,
+                    "Full Stack Developer",
+                    2000,
+                    "DevOps Engineer",
+                    2000,
+                  ]}
+                />
+              </p>
+              <div className="mt-4">
+                <AwesomeButtonProgress
+                  type="secondary"
+                  size="medium"
+                  action={(element, next) => {
+                    setTimeout(() => {
+                      next();
+                      doSomethingThenCall();
+                    }, 1000);
+                  }}
+                >
+                  Download CV
+                </AwesomeButtonProgress>
+              </div>
             </div>
           </div>
+          <div className="bg-green-500 relative md:row-span-3 sm:row-span-4 lg:row-span-1 xl:row-span-1 row-span-4">
+                        <img className="absolute h-full w-full object-cover" src={caneral} alt="caneral" />
+                    </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        portfolio: '#251170',
+        line: '##eb83f8'
+      }
+    },
+    
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
